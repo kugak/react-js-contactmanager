f3aabe54cde17965898ae132103d81deb0c5aab7
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-02-26 13:57:24 -0500

    added bootstrap and updated UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -8,12 +9,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="Karen@gmail.com"
-          phone="555-666-5555"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="Karen@gmail.com"
+            phone="555-666-5555"
+          />
+        </div>
       </div>
     );
   }
