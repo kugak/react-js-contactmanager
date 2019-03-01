9278f4f9d523ae4f61b5416fa61cc634a0549134
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-03-01 11:37:47 -0500

    added react router and routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
-import Header from "./components/layouts/Header";
 import AddContact from "./components/contacts/AddContact";
 import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/layouts/Header";
+import About from "./components/pages/About";
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact/add" component={AddContact} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
