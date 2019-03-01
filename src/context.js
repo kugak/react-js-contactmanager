e32e1bb5066807fd94e043e03c6db37ed2c2bccb
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-03-01 15:50:15 -0500

    added axios and updated contacts to use placeholder users from api

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -23,30 +24,17 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "kwilliams@gmail.com",
-        phone: "555-666-5555"
-      },
-      {
-        id: 3,
-        name: "Hendry Man",
-        email: "hman@gmail.com",
-        phone: "555-555-7777"
-      }
-    ],
+    contacts: [],
     dispatch: action => {
       this.setState(state => reducer(state, action));
     }
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => this.setState({ contacts: res.data }));
+  }
 
   render() {
     return (
